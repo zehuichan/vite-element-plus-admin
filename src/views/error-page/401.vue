2c67bd0f457edd13c3d29bb411e482cb915422ae
zehuichan <jasonchenzehui@gmail.com>
2022-04-02 17:14:23 +0800

    feat: 错误页面跳转方法调整

--- src/views/error-page/401.vue
+++ src/views/error-page/401.vue
@@ -4,14 +4,14 @@
       <div class="title">401</div>
       <div class="info">抱歉，你没有权限访问该页面</div>
       <div class="button-wrapper">
-        <el-button type="primary" size="mini" @click="$router.push('/')">返回首页</el-button>
+        <el-button type="primary" @click="router.push('/')">返回首页</el-button>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-  export default {
-    name: 'Page401'
-  }
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 </script>
--- src/views/error-page/500.vue
+++ src/views/error-page/500.vue
@@ -4,14 +4,14 @@
       <div class="title">500</div>
       <div class="info">抱歉，服务器出错了</div>
       <div class="button-wrapper">
-        <el-button type="primary" size="mini" @click="$router.push('/')">返回首页</el-button>
+        <el-button type="primary" @click="router.push('/')">返回首页</el-button>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-  export default {
-    name: 'Page500'
-  }
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 </script>
