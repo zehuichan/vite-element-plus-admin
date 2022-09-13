f0523f32e6fb3564355a1573ff309177e7413701
Zehui Chan <jasonchenzehui@gmail.com>
2022-09-13 11:22:12 +0800

    feat: SchemaForm支持第一个input获得焦点

---
+++ src/components/SchemaForm/src/hooks/useAutoFocus.js
@@ -0,0 +1,31 @@
+import { nextTick, unref, watchEffect } from 'vue'
+
+export async function useAutoFocus({
+  getSchema,
+  getProps,
+  formElRef,
+  isInitedDefault
+}) {
+  watchEffect(async () => {})
+
+  if (unref(isInitedDefault) || !unref(getProps).autoFocusFirstItem) {
+    return
+  }
+  await nextTick()
+  const schemas = unref(getSchema)
+  const formEl = unref(formElRef)
+  const el = formEl?.$el
+  if (!formEl || !el || !schemas || schemas.length === 0) {
+    return
+  }
+
+  const firstItem = schemas[0]
+  // Only open when the first form item is input type
+  if (!firstItem.component.includes('Input')) {
+    return
+  }
+
+  const inputEl = el.querySelector('.el-row:first-child input')
+  if (!inputEl) return
+  inputEl?.focus()
+}
