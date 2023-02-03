7ad47266dd7a001bcc858acfa3a387cd50fbee6c
zehuichan <zehuichan@users.noreply.github.com>
2023-02-03 08:53:56 +0000

    deploy: 9ad2ce4ccda28010a319d46a81fdb9f343add36d

---
+++ assets/index-b5e6f91c.js
@@ -0,0 +1,1 @@
+import{K as r}from"./index-ab2fab3a.js";function o(){return r.request({url:"/error",method:"get"})}function i(t){return r.request({url:`/dict/${t}`,method:"get"})}export{i as d,o as f};
