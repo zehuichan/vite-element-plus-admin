74126cf406ee0302eac555618de6fb855779bfb4
zehuichan <zehuichan@users.noreply.github.com>
2023-02-20 02:01:30 +0000

    deploy: aaf600eb38e68382622a4cc55fc60c8689489f31

--- assets/index-64450fdb.js
+++
@@ -1,1 +0,0 @@
-import{K as r}from"./index-d8cc8a33.js";function o(){return r.request({url:"/error",method:"get"})}function i(t){return r.request({url:`/dict/${t}`,method:"get"})}export{i as d,o as f};
