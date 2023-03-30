bdac037635115cef248647dee7480579db12f864
Zehui Chan <jasonchenzehui@gmail.com>
2023-03-30 16:21:59 +0800

    feat: unocss

--- src/views/feat/unocss/index.vue
+++ src/views/feat/unocss/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app-container">
     <div class="w-100px h-100px p-10px text-white bg-amber">unocss</div>
+    <div class="p-5px mt-[10px] bg-[#b2a8bb]">unocss</div>
   </div>
 </template>
 
--- src/views/redirect/index.vue
+++ src/views/redirect/index.vue
@@ -2,35 +2,30 @@
   <div></div>
 </template>
 
-<script>
-import { defineComponent, unref } from 'vue'
+<script setup>
+import { unref } from 'vue'
 import { useRouter } from 'vue-router'
 
-export default defineComponent({
-  name: 'Redirect',
-  setup() {
-    const { currentRoute, replace } = useRouter()
+const { currentRoute, replace } = useRouter()
 
-    const { params, query } = unref(currentRoute)
-    const { path, _redirect_type = 'path' } = params
+const { params, query } = unref(currentRoute)
+const { path, _redirect_type = 'path' } = params
 
-    Reflect.deleteProperty(params, '_redirect_type')
-    Reflect.deleteProperty(params, 'path')
+Reflect.deleteProperty(params, '_redirect_type')
+Reflect.deleteProperty(params, 'path')
 
-    const _path = Array.isArray(path) ? path.join('/') : path
+const _path = Array.isArray(path) ? path.join('/') : path
 
-    if (_redirect_type === 'name') {
-      replace({
-        name: _path,
-        query,
-        params
-      })
-    } else {
-      replace({
-        path: _path.startsWith('/') ? _path : '/' + _path,
-        query
-      })
-    }
-  }
-})
+if (_redirect_type === 'name') {
+  replace({
+    name: _path,
+    query,
+    params
+  })
+} else {
+  replace({
+    path: _path.startsWith('/') ? _path : '/' + _path,
+    query
+  })
+}
 </script>
