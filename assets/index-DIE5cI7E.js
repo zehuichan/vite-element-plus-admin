9afad34bb6cdebc7fc18afbc0f62e01ea2d5ff8e
zehuichan <zehuichan@users.noreply.github.com>
2024-03-28 14:04:27 +0000

    deploy: 0709963a548f2f4ec3c979f62bcb74c3db2bd080

---
+++ assets/index-DIE5cI7E.js
@@ -0,0 +1,1 @@
+import{k as n,a as c,o as p,h as u}from"./index-BHlah-Nh.js";const l={__name:"index",setup(_){const{currentRoute:r,replace:a}=n(),{params:s,query:o}=c(r),{path:t}=s,e=Array.isArray(t)?t.join("/"):t;return a({path:e.startsWith("/")?e:"/"+e,query:o}),(i,h)=>(p(),u("div"))}};export{l as default};
